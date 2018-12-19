68bbc7273854cd81f5a0804787ebc0c3137986f1
Taylor Hulsmans <thulsmans.2994@me.com>
2018-12-18 22:19:49 -0700

    begginning fabric setup

--- setup/start-root-ca.sh
+++ setup/start-root-ca.sh
@@ -7,7 +7,7 @@ set -e
 echo $BOOTSTRAP_USER_PASS
 # Initialize the root CA
 fabric-ca-server init -b $BOOTSTRAP_USER_PASS
-echo $TARGET_CERFILE
+echo $TARGET_CERTFILE
 # Copy the root CA's signing certificate to the data directory to be used by others
 cp $FABRIC_CA_SERVER_HOME/ca-cert.pem $TARGET_CERTFILE
 
@@ -20,4 +20,4 @@ sed -i "/affiliations:/a \\   $aff" \
    $FABRIC_CA_SERVER_HOME/fabric-ca-server-config.yaml
 
 # Start the root CA
-fabric-ca-server start
+fabric-ca-server start -b admin-ordererorg:admin-ordererorgpw -d
